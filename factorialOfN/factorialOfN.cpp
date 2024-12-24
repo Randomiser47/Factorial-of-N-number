3eca19a736ca089bc2118f9136e6ba5391ea3cb2
Hassaan Alvi <110996624+Randomiser47@users.noreply.github.com>
2024-12-24 21:16:44 +0500

    Add files via upload

---
+++ factorialOfN/factorialOfN.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i;
+    int n =9;
+    int mul =1;
+    int all;
+    for (i=1; i<=n; i++)
+    {
+        mul = mul*i;
+        // all = mul;
+       
+        
+    }
+    cout<< mul <<endl;
+
+    return 0;
+}
